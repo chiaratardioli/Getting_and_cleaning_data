0fc70784f8e411c257313d094b0f0e8c005ae3a0
chiaratardioli <c.tardioli@strath.ac.uk>
2015-05-24 16:14:43 +0100

    My project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Getting_and_cleaning_data
-Peer Assessments
+---
+title: "Getting and cleaning data: Peer Assessment 1"
+output: 
+  html_document:
+    keep_md: true
+---
+
+## Download data for the project.
+The data for the project have been collected from the accelerometers from the Samsung Galaxy S smartphone. They have been downloaded from the following weblink
+
+[https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zips](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+## Understanding the data
+The [code book](link here) describes the variables names and the data in each files.
+
+## Analysis
+The R code [Run analysis](link here) performes an analysis on the data collected on the activity measurements. The goal is to extracts only the variables related to the mean and standard deviation for each measurement and to create a tiny dataset containing the average of variable for each activity and each subject.
+
+## File in this repository:
+- `README.md`
+- `CodeBook.md` and `CodeBook.Rmd`
+- `run_analysis.R`
---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+# RUN ANALYSIS.R
+# Note: The input datasets are obtained by cleaning up the data provided in UCI HAR Dataset.
+#       Look at the CodeBook.md for more info.
+
+library(dplyr)
+
+# 1. Merges the training and the test sets to create one data set.
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt") # read training data
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")    # read test data
+x_data <- rbind(x_train,x_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("id","name")) 
+logical_vector <- grepl("mean\\(",features$name) | grepl("std\\(",features$name)
+x_data <- x_data[,logical_vector]
+
+# 3. Uses descriptive activity names to name the activities in the data set.
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = c("activity.label"))
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c("activity.label") )
+y_data <- rbind(y_train,y_test)
+
+activity_names <- function(i){
+    activity_vector = c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS",
+                        "SITTING","STANDING","LAYING")
+    activity_vector[i]
+}
+y_data <- mutate(y_data, activity.label=activity_names(activity.label))
+
+# 4. Appropriately labels the data set with descriptive variable names.
+features.name <- features$name[logical_vector]
+colnames(x_data)[1:length(features.name)] <- as.character(features.name)
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the 
+# average of each variable for each activity and each subject.
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = c("subject.ID"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = c("subject.ID"))
+s_data <- rbind(subject_train,subject_test)  # subject identifier column
+
+mergeData <- cbind(x_data,y_data,s_data)  # all the info in a single fata frame
+
+groupData <- aggregate(. ~subject.ID + activity.label, mergeData, mean)
+groupData <- groupData[order(groupData$subject.ID,groupData$activity.label),]
+write.table(groupData, file = "tidy_dataset.txt", row.name = FALSE)
+
+
